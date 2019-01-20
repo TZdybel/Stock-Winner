2ddfd184a4577d2a7ec0e7f99846dd5f4dd04f28
Bartosz Sofiński <bartoszsofinski@gmail.com>
2019-01-20 22:00:11 +0100

    getResults poprawione

--- src/main/java/stockwinner/logic/Strategy.java
+++ src/main/java/stockwinner/logic/Strategy.java
@@ -28,22 +28,38 @@ public class Strategy {
         this.name = name;
     }
 
-    public List<Double> getResults(List<Double> input, double startingCash){
+    public List<Double> getResults(List<Double> input, double cash){
         // decyzja strategii jest procentem naszych środków które zainwestujemy/sprzedamy
         // ale gdyby zrobić obiekt "portfel" to miałby jedną metodę i zero pól
         // więc strategia od razu symuluje środki w naszym portfelu i zwraca $$$
         //StrategyPart - parts
 
-        double toInvest = 0;
-        ArrayList<Double> res = new ArrayList<>();
-        for(int i=0; i<input.size(); i++){
-            toInvest = 0;
-            for(int j=0; j<parts.size(); j++){
-                toInvest += parts.get(j).getValue(input, i); //w %
+        double invested = 0;
+        ArrayList<Double> results = new ArrayList<>();
+        for(int i = 1; i < input.size(); i++){
+
+            double decision = 0.0;
+            for(int j = 0; j < parts.size(); j++){
+                decision += parts.get(j).getValue(input, i);
             }
-            res.add(startingCash*toInvest*0.01+input.get(i));
+
+            // zainwestowane środki rosną/maleją
+            invested *= input.get(i) / input.get(i-1);
+
+            // a potem dodajemy/odejmujemy od nich na podstawie decyzji
+            if( decision > 0 ){
+                double delta = cash * decision/100.0;
+                cash -= delta;
+                invested += delta;
+            } else {
+                double delta = invested * decision/100.0;
+                cash += delta;
+                invested -= delta;
+            }
+
+            results.add(cash + invested);
         }
-        return res;
+        return results;
     }
 
 }
